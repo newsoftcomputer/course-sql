a20a6301d90f4d50931d51ca757df0f6fc4f341f
Andres Giraldo <andresganc@gmail.com>
2023-06-29 16:52:43 -0500

    New: Sintaxis, Select All, Select Column, Select Distinct, Where

---
+++ course-nc/01-basic/03-select/02-select-column.sql
@@ -0,0 +1,4 @@
+
+SELECT column1, column2, column3 FROM table_name
+
+SELECT CustomerName, City FROM Customers;
---
+++ course-nc/01-basic/05-where/example-01.sql
@@ -0,0 +1,10 @@
+
+/* Sintaxis */
+SELECT column1, column2 FROM table_name WHERE condition;
+
+
+/* Example 01 */
+SELECT * FROM Customers WHERE Country='Mexico';
+
+/* Example 02 */
+SELECT CustomerName, City FROM Customers WHERE Country='Mexico';
